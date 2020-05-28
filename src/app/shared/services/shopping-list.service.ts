8961ddc90b1368b25863637ae62db7777f4ee663
Jyoti Mishra <jyoti@flytbase.com>
2020-05-29 00:38:05 +0530

    added shopping list service for shopping-list and shopping list edit

--- src/app/shared/services/shopping-list.service.ts
+++ src/app/shared/services/shopping-list.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+
+import {Ingredient} from '../ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Flour', 1),
+    new Ingredient('choco', 5)
 
+  ];
+
+  ingredientChanged = new EventEmitter<Ingredient[]>();
+
+  getIngredient() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(item: Ingredient) {
+    this.ingredients.push(item);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
   constructor() { }
 }
