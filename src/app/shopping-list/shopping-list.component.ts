0ec606a40c1d19488610eb9b8509d496f1b233e7
Jyoti Mishra <jyoti@flytbase.com>
2020-05-29 00:39:35 +0530

    using service in shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
+import { ShoppingListService } from '../shared/services/shopping-list.service';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Flour', 1),
-    new Ingredient('choco', 5)
 
-  ];
-  constructor() { }
+  ingredients: Ingredient[];
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.shoppingListService.getIngredient();
+    this.shoppingListService.ingredientChanged.subscribe(( ingredients ) => this.ingredients = ingredients);
   }
 
   onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    // this.ingredients.push(ingredient);
+
+    this.shoppingListService.addIngredient(ingredient);
+
   }
 
 }
