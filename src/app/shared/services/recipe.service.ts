c986c46c64f64b7e0f9e13adc5a0f481dbf9ad80
Jyoti Mishra <jyoti@flytbase.com>
2020-05-29 01:05:03 +0530

    adding ingredient data to centralised Recipe Service

--- src/app/shared/services/recipe.service.ts
+++ src/app/shared/services/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import {Recipe} from '../../recipes/recipe.model';
+import { Ingredient } from '../ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,16 @@ import {Recipe} from '../../recipes/recipe.model';
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Ice Cream', 'IceCream with chocolate', 'https://images.pexels.com/photos/1739347/pexels-photo-1739347.jpeg'),
-    new Recipe('Test', 'test test taste test', 'https://images.pexels.com/photos/1739347/pexels-photo-1739347.jpeg')
+    new Recipe('Ice Cream', 'IceCream with chocolate', 'https://images.pexels.com/photos/1739347/pexels-photo-1739347.jpeg', [
+      new Ingredient('chocolateChips', 20),
+      new Ingredient('Chocolate-sauce', 8),
+      new Ingredient('milk', 2)
+    ]),
+    new Recipe('Test', 'test test taste test', 'https://images.pexels.com/photos/1739347/pexels-photo-1739347.jpeg', [
+      new Ingredient('chocolateChips', 20),
+      new Ingredient('Chocolate-sauce', 8),
+      new Ingredient('milk', 2)
+    ])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
