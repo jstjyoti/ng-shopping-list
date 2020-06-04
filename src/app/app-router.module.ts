d40f1897f5489e2e3ee0e313c8ea8553ff60e0f5
Jyoti Mishra <jyoti@flytbase.com>
2020-06-04 14:26:08 +0530

    child routes

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -4,10 +4,15 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {RecipesComponent} from '../app/recipes/recipes.component';
 import {ShoppingListComponent} from '../app/shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoute: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent},
+    {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent},
+        {path: ':id', component: RecipeDetailComponent }
+    ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
