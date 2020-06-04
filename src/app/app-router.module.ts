64b4a38134596a5a1e037b625ac05fc418b1eef1
Jyoti Mishra <jyoti@flytbase.com>
2020-06-04 12:01:53 +0530

    adding router module

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
+import {RecipesComponent} from '../app/recipes/recipes.component';
+import {ShoppingListComponent} from '../app/shopping-list/shopping-list.component';
+
+const appRoute: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', component: RecipesComponent},
+    {path: 'shopping-list', component: ShoppingListComponent}
+];
+
 @NgModule({
-    imports: []
+    imports: [RouterModule.forRoot(appRoute)],
+    exports: [RouterModule]
 }) export class AppRouterModule {
 
 }
